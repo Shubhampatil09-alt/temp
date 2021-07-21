4f31a9c9d1fe2253b95e4d2349e8a83ddb0cd94d
Shubhampatil09-alt <84279121+Shubhampatil09-alt@users.noreply.github.com>
2021-07-21 13:37:13 +0530

    Add files via upload

---
+++ calculator_module_demo.py
@@ -0,0 +1,19 @@
+# user define module demo
+'''
+#calculator:module
+my custome module calculator.\n 
+pass parameter this way -->\n
+obj.method_name(param1, param2)
+'''
+def addition(no1,no2):
+    print(f"Addition of       {no1} + {no2} = {no1+no2}\n")
+
+def substraction(no1,no2):
+    print(f"Substraction of   {no1} - {no2} = {no1-no2}\n")
+
+def multiplication(no1,no2):
+    print(f"Multiplication of {no1} * {no2} = {no1*no2}\n")
+
+def division(no1,no2):
+    print(f"division of       {no1} / {no2} = {no1/no2}\n")
+
